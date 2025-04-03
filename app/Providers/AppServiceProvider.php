d966037e08a59910a4fe314863f9814ae7c2eac9
Rob Mellett <dev@robmellett.com>
2025-04-03 13:15:51 +0700

    Wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\AnalyticsService;
+use App\Services\HttpAnalyticsGateway;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->singleton(AnalyticsService::class, function (Application $app) {
+            return HttpAnalyticsGateway::make();
+        });
     }
 }
--- tests/Feature/AnalyticsServiceTest.php
+++ tests/Feature/AnalyticsServiceTest.php
@@ -7,6 +7,7 @@ namespace Tests\Feature;
 use App\Services\AnalyticsService;
 use App\Services\HttpAnalyticsGateway;
 use App\Services\InMemoryGateway;
+use Illuminate\Support\Facades\App;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -15,6 +16,10 @@ class AnalyticsServiceTest extends TestCase
     #[Test]
     public function can_create_the_service(): void
     {
+        $service = App::make(AnalyticsService::class);
+
+        $this->assertInstanceOf(HttpAnalyticsGateway::class, $service);
+
         $this->assertInstanceOf(AnalyticsService::class, HttpAnalyticsGateway::make());
 
         $this->assertInstanceOf(HttpAnalyticsGateway::class, HttpAnalyticsGateway::make());
